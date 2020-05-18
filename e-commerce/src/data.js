d72558f824d56515e8d7f9bd9989d2aa3ef0dd21
Anushka Darade <anushkadarade@gmail.com>
2020-05-19 03:06:03 +0530

    React app

---
+++ e-commerce/src/data.js
@@ -0,0 +1,57 @@
+export default {
+    products: [
+        {
+        name:'Round Neck',
+        category:'Tshirts',
+        image:'/images/a1.jpg',
+        price:60,
+        brand:'Puma',
+        rating:4.5,
+        numReviews: 10
+        },
+        {
+            name:'Polo T-shirt',
+            category:'Tshirts',
+            image:'/images/a2.jpg',
+            price:105,
+            brand:'Puma',
+            rating:4,
+            numReviews: 8
+        },
+        {
+            name:'T-shirt',
+            category:'Tshirts',
+            image:'/images/a3.jpg',
+            price:95,
+            brand:'Reebok',
+            rating:3.5,
+            numReviews: 5
+        },
+        {
+            name:'Trousers',
+            category:'Pants',
+            image:'/images/a4.jpg',
+            price:135,
+            brand:'Raymond',
+            rating:4.8,
+            numReviews: 12
+        },{
+            name:'Capri',
+            category:'Pants',
+            image:'/images/a5.jpg',
+            price:112,
+            brand:'AJIO',
+            rating:4.2,
+            numReviews: 8
+        },{
+            name:'Joggers',
+            category:'Pants',
+            image:'/images/a6.jpg',
+            price:70,
+            brand:'Reebok',
+            rating:3.9,
+            numReviews: 5
+        },
+
+]
+}
